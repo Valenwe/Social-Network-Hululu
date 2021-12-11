66f684fceb01a706769b2e35ceeeeb28ca49cde5
Louis Vié <62017711+Valenwe@users.noreply.github.com>
2021-12-11 16:26:26 +0000

    Update db.php

--- backend/db.php
+++ backend/db.php
@@ -3,6 +3,6 @@
 // connect to the database
 // root -> password
 // mysql.exe -u root -p
-$db = mysqli_connect('localhost', 'root', 'root', 'hululu');
+$db = mysqli_connect('localhost', 'root', '', 'hululu');
 
 if (!$db) die("Connection failed: " . mysqli_connect_error());
